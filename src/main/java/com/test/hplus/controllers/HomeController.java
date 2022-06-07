f2d1179db30123438789db9936512da4b2ca7263
Ivan <ivanfflores22@gmail.com>
2022-06-07 14:26:41 -0700

    first commit

---
+++ src/main/java/com/test/hplus/controllers/HomeController.java
@@ -0,0 +1,20 @@
+package com.test.hplus.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/home")
+    public String goHome(){
+        System.out.println("in home controller");
+        return "index";
+    }
+
+    @GetMapping("/goToSearch")
+    public String goToSearch(){
+        System.out.println("goint to search page");
+        return "search";
+    }
+}
